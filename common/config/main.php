c4ee7e95f883fa4fe98c1903624358d7b1472f01
Vcent <Vcent@DESKTOP-J2PUEBG>
2018-08-28 12:35:16 -0400

    1.- primer comitt Eugene

---
+++ common/config/main.php
@@ -0,0 +1,52 @@
+<?php
+return [
+    'id'=>'Luxor',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'modules'=>[
+      'admin'=>[
+            'class'=>'mdm\admin\module',
+      ],
+        'gridview'=>[
+        'class'=> \kartik\grid\GridView::className()  
+        ],
+    ],
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'authManager'=>[
+            'class'=>'yii\rbac\DBManager'
+           ],
+        'user' => [
+            'identityClass' => 'mdm\admin\models\User',
+            "loginUrl" => ['admin/user/login'],
+            //'enableAutoLogin' => true,
+            
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+    ],
+];
---
+++ translations/es/timothy.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+return[
+     
+   'contacts timothy' => 'contatos de timothy',
+    'god'=> 'dios',
+    'fullname' => 'Nombre completo',
+    
+    
+];
